f1d37b00e66345c40c33ae6b65ff99825d09ed03
Ilya Radchenko <ilya@burstcreations.com>
2014-03-18 10:56:32 -0400

    ops

---
+++ index.js
@@ -0,0 +1,17 @@
+var Option = require('./lib/option');
+var Command = require('./lib/command');
+
+/**
+ * Expose the root command.
+ */
+exports = module.exports = new Command();
+
+/**
+ * Expose `Command`.
+ */
+exports.Command = Command;
+
+/**
+ * Expose `Option`.
+ */
+exports.Option = Option;
